c2408334be3f05bea9655b5e21424018500efec0
TaleaX <sophie.dehne01@gmail.com>
2022-06-04 10:42:26 +0200

    update

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: tdehne <tdehne@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 14:19:35 by tdehne            #+#    #+#             */
-/*   Updated: 2022/05/22 08:50:53 by tdehne           ###   ########.fr       */
+/*   Updated: 2022/05/22 15:22:03 by tdehne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ void	print_rect(t_data *img, int x, int y, int width, int color)
 	}
 }
 
-void calc(double re_a, double im_b, double re_c, double im_d, int depth, int* counter, t_data *img)
+/*void calc(double re_a, double im_b, double re_c, double im_d, int depth, int* counter, t_data *img)
 {
 	double	re;
 	double	im;
@@ -63,30 +63,69 @@ void calc(double re_a, double im_b, double re_c, double im_d, int depth, int* co
 	}
 	(*counter)++;
 	calc(re_a, im_b, re, im, depth, counter, img);
+}*/
+
+void calc(double re_a, double im_b, double re_c, double im_d, int depth, int px, int py)
+{
+	double	re;
+	double	im;
+	int counter;
+
+	counter = 0;
+	while (1)
+	{
+		re = re_c*re_c - im_d*im_d + re_a;
+		im = 2*re_c*im_d + im_b;
+		printf("re = %.1f im =  %.1f re_a =  %.1f im_b =  %.1f px = %d py = %d betrag = %.1f\n", re, im, re_a, im_b, px, py, sqrt(re * re + im * im));
+		if (sqrt(re*re + im*im) > 2.0){
+			printf("greater 2 \n");
+			break ;
+		}
+		if (counter == depth){
+			printf("in here");
+			break;
+		}
+		re_c = re;
+		im_d = im;
+		counter++;
+	}
 }
 
 int main()
 {
-	void	*mlx;
+	/*void	*mlx;
 	t_data	img;
-	void	*mlx_win;
+	void	*mlx_win;*/
 	int		win_width;
 	int		win_height;
+	double	neg;
+	double	pos;
+	double	steps;
 
-	win_width = 800;
-	win_height = 800;
-	mlx = mlx_init();
+	win_width = 400;
+	win_height = 400;
+	/*mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, win_width, win_height, "Hello world!");
 	img.img = mlx_new_image(mlx, 10, 10);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);*/
 	
 	//mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	int counter;
-
-	counter = 0;
-	calc(1, 0, 0, 0, 50, &counter, &img);
+	neg = -2;
+	steps = 1.0 / (win_height / 4);
+	printf("steps = %.3f\n", steps);
+	for (int i = 0; i < win_width; i++)
+	{
+		pos = 2;
+		for (int j = 0; j < win_height; j++)
+		{
+			calc(neg, pos, 0, 0, 10, i, j);
+			pos -= steps;	
+		}
+		neg += steps;
+	}
+	/*calc(1, 0, 0, 0, 50, &counter, &img);
 	printf("%d\n", counter);
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
+	mlx_loop(mlx);*/
 	return (0);
 }
