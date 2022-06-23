cfa0795371d7e17f11a374be8194615a20609e85
TaleaX <sophie.dehne01@gmail.com>
2022-06-23 13:34:29 +0200

    update

--- src/test.c
+++ src/test.c
@@ -6,7 +6,7 @@
 /*   By: tdehne <tdehne@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 07:19:22 by tdehne            #+#    #+#             */
-/*   Updated: 2022/06/22 14:46:13 by tdehne           ###   ########.fr       */
+/*   Updated: 2022/06/23 13:31:18 by tdehne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,25 +73,6 @@ void calc(double c_r, double c_i, double z_r, double z_i, int px, int py, int de
 		re = z_r*z_r - z_i*z_i + c_r;
 		im = 2*z_r*z_i + c_i;
 		//printf("re = %.1f im =  %.1f c_r =  %.1f c_i =  %.1f px = %d py = %d betrag = %.1f\n", re, im, c_r, c_i, px, py, sqrt(re * re + im * im));
-		if (sqrt(re*re + im*im) > 2.0){
-			//mlx_pixel_put(mlx, mlx_win, px, py, 0x00FF66FF);
-			//printf("c_r = %.3f c_i = %.3f z_r = %.3f z_i = %.3f greater 2 \n", c_r, c_i, z_r, z_i);
-			if (counter > 10)
-				my_mlx_pixel_put(img, px, py, MAGENTA);
-			else if (counter > 8)
-				my_mlx_pixel_put(img, px, py, RED);
-			else if (counter > 7)
-				my_mlx_pixel_put(img, px, py, ROSA);
-			else if (counter > 6)
-				my_mlx_pixel_put(img, px, py, PURPLE);
-			else if (counter > 4)
-				my_mlx_pixel_put(img, px, py, BLUE);
-			else if (counter > 2)
-				my_mlx_pixel_put(img, px, py, LIGHT_PURPLE);
-			else
-				my_mlx_pixel_put(img, px, py, LIGHT_BLUE);
-			break ;
-		}
 		if (counter == depth){
 			//printf("in here");
 			//mlx_pixel_put(mlx, mlx_win, px, py, 0x00000000);
@@ -99,6 +80,26 @@ void calc(double c_r, double c_i, double z_r, double z_i, int px, int py, int de
 			img_pix_put(img, px, py, 0x00000000);
 			break;
 		}
+		if (sqrt(re*re + im*im) > 2.0){
+			//mlx_pixel_put(mlx, mlx_win, px, py, 0x00FF66FF);
+			//printf("c_r = %.3f c_i = %.3f z_r = %.3f z_i = %.3f greater 2 \n", c_r, c_i, z_r, z_i);
+			if (counter > 45)
+				my_mlx_pixel_put(img, px, py, MAGENTA);
+			else if (counter > 40)
+				my_mlx_pixel_put(img, px, py, RED);
+			else if (counter > 30)
+				my_mlx_pixel_put(img, px, py, ROSA);
+			else if (counter > 20)
+				my_mlx_pixel_put(img, px, py, PURPLE);
+			else if (counter > 10)
+				my_mlx_pixel_put(img, px, py, BLUE);
+			else if (counter > 5)
+				my_mlx_pixel_put(img, px, py, LIGHT_PURPLE);
+			else
+				my_mlx_pixel_put(img, px, py, LIGHT_BLUE);
+			break ;
+		}
+		
 		z_r = re;
 		z_i = im;
 		counter++;
@@ -112,33 +113,30 @@ int main()
 	t_img	img;
 	double	neg;
 	double	pos;
-	double	steps;
+	double	steps_x;
+	double	steps_y;
 	int		win_width;
 	int		win_height;
-	int		img_width;
-	int		img_height;
 
 	mlx = mlx_init();
-	win_height = 1920;
-	win_width = 1920;
-	img_height = 100;
-	img_width = 100;
+	win_height = 1300;
+	win_width = 1350;
 	mlx_win = mlx_new_window(mlx, win_width, win_height, "Hello world!");
 	img.img = mlx_new_image(mlx, win_width, win_height);
 	img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.line_length, &img.endian);
 	neg = -2;
-	steps =  4.0 / 1920.0;
+	steps_x =  4.0 / win_width;
+	steps_y = 4.0 / win_height;
 	//printf("steps = %.3f\n", steps);
 	for (int i = 0; i < win_width; i++)
 	{
 		pos = 2;
 		for (int j = 0; j < win_height; j++)
 		{
-			calc(neg, pos, 0, 0, i, j, 500, &img);
-			pos -= steps;
+			calc(neg, pos, 0, 0, i, j, 50, &img);
+			pos -= steps_y;
 		}
-		neg += steps;
-		printf("steps %f pos %f neg %f\n", steps, pos, neg);
+		neg += steps_x;
 	}
 	//print_rect(&img, 100, 100, 20, 0x00FF03DD);
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
