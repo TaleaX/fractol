34bbd0e07891651b21b11fd37f13af0151439c77
TaleaX <sophie.dehne01@gmail.com>
2022-08-25 20:36:56 +0200

    added color2

--- src/coloring_alg.c
+++ src/coloring_alg.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   coloring.c                                         :+:      :+:    :+:   */
+/*   coloring_alg.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tdehne <tdehne@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 17:30:08 by tdehne            #+#    #+#             */
-/*   Updated: 2022/08/25 17:06:34 by tdehne           ###   ########.fr       */
+/*   Updated: 2022/08/25 20:35:27 by tdehne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,36 @@ int	color(int n, int max_value, t_color color)
 	int		g;
 	int		b;
 
+	a = color.width * n / max_value + M_PI / 2 + color.offset;
+	if (color.reverse)
+		a = color.width * (max_value - n) / max_value + M_PI / 2 + color.offset;
+	r = sin(a) * 192 + 128;
+	if (r < 0)
+		r = 0;
+	else if (r > 255)
+		r = 255;
+	g = sin(a - (2 * M_PI) / 3) * 192 + 128;
+	if (g < 0)
+		g = 0;
+	else if (g > 255)
+		g = 255;
+	b = sin(a - (4 * M_PI) / 3) * 192 + 128;
+	if (b < 0)
+		b = 0;
+	else if (b > 255)
+		b = 255;
+	return (get_rgba(r, g, b, 255));
+}
+
+int	color2(int n, int max_value, t_color color)
+{
+	double	a;
+	int		r;
+	int		g;
+	int		b;
+
+	if (n == 1)
+		n = 0;
 	a = color.width * n / max_value + M_PI / 2 + color.offset;
 	if (color.reverse)
 		a = color.width * (max_value - n) / max_value + M_PI / 2 + color.offset;
